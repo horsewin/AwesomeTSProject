c328f4a0062d9e5f71bc9016a871ce31e338c967
AtsushiUMAKATSU <mamamatz@gmail.com>
2020-03-30 14:55:55 +0900

    modify

--- settings.tsx
+++ settings.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import messaging from '@react-native-firebase/messaging';
-import {StyleSheet, Text, View} from 'react-native';
+import {Alert, Button, Platform, StyleSheet, Text, View} from 'react-native';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 interface IState {
@@ -9,15 +9,15 @@ interface IState {
   token: string;
 }
 
-const sleep = (msec: number) => {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve();
-    }, msec);
-  });
-};
-
+/**
+ *
+ */
 export default class Settings extends React.Component<{}, IState> {
+  /**
+   *
+   * @param props
+   * @param state
+   */
   constructor(props: any, state: any) {
     super(props, state);
     this.state = {
@@ -26,9 +26,23 @@ export default class Settings extends React.Component<{}, IState> {
       token: '',
     };
   }
+
+  /**
+   *
+   * @returns {Promise<void>}
+   */
   public async componentDidMount(): Promise<void> {
-    await messaging().registerForRemoteNotifications();
-    await this.requestPermission();
+    if (
+      Platform.OS === 'ios' &&
+      !messaging().isRegisteredForRemoteNotifications
+    ) {
+      await messaging().registerForRemoteNotifications();
+    }
+
+    const hasPermission = await messaging().hasPermission();
+    if (!hasPermission) {
+      await this.requestPermission();
+    }
 
     const fcmToken = await messaging().getToken();
     const token = await messaging().getAPNSToken();
@@ -40,9 +54,9 @@ export default class Settings extends React.Component<{}, IState> {
     const granted = await messaging().requestPermission();
 
     if (granted) {
-      console.log('User granted messaging permissions!');
+      Alert.alert('通知許可', '許可しました');
     } else {
-      console.log('User declined messaging permissions :(');
+      Alert.alert('通知許可', '拒否しました');
     }
   };
 
@@ -65,6 +79,21 @@ export default class Settings extends React.Component<{}, IState> {
         <Text style={styles.sectionDescription}>
           {this.state.loading ? this.state.token : 'loading...'}
         </Text>
+
+        <Button
+          title={'トークン更新'}
+          onPress={async () => {
+            try {
+              const fcmToken = await messaging().getToken();
+              const token = await messaging().getAPNSToken();
+
+              this.setState({loading: true, fcmToken, token: token || 'null'});
+            } catch (e) {
+              Alert.alert('Error', e.message);
+              this.setState({loading: true, fcmToken: 'error', token: 'error'});
+            }
+          }}
+        />
       </View>
     );
   }
