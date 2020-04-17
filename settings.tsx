a503cd1571bdd79ee903a500cb149726be55dd6c
AtsushiUMAKATSU <mamamatz@gmail.com>
2020-04-18 01:01:09 +0900

    at once

--- settings.tsx
+++ settings.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import messaging from '@react-native-firebase/messaging';
+//import analytics from '@react-native-firebase/analytics';
 import {Alert, Button, Platform, StyleSheet, Text, View} from 'react-native';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 interface IState {
   loading: boolean;
+  isRegistered: boolean;
+  hasPermission: boolean;
   fcmToken: string;
   token: string;
 }
@@ -22,6 +25,8 @@ export default class Settings extends React.Component<{}, IState> {
     super(props, state);
     this.state = {
       loading: false,
+      isRegistered: false,
+      hasPermission: false,
       fcmToken: '',
       token: '',
     };
@@ -32,10 +37,8 @@ export default class Settings extends React.Component<{}, IState> {
    * @returns {Promise<void>}
    */
   public async componentDidMount(): Promise<void> {
-    if (
-      Platform.OS === 'ios' &&
-      !messaging().isRegisteredForRemoteNotifications
-    ) {
+    const isRegistered = messaging().isRegisteredForRemoteNotifications;
+    if (Platform.OS === 'ios' && !isRegistered) {
       await messaging().registerForRemoteNotifications();
     }
 
@@ -47,7 +50,13 @@ export default class Settings extends React.Component<{}, IState> {
     const fcmToken = await messaging().getToken();
     const token = await messaging().getAPNSToken();
 
-    this.setState({loading: true, fcmToken, token: token || 'null'});
+    this.setState({
+      loading: true,
+      fcmToken,
+      token: token || 'null',
+      hasPermission,
+      isRegistered,
+    });
   }
 
   private requestPermission = async () => {
@@ -62,39 +71,94 @@ export default class Settings extends React.Component<{}, IState> {
 
   public render() {
     return (
-      <View style={styles.sectionContainer}>
-        <Text style={styles.sectionTitle}>Data status</Text>
-        <Text style={styles.sectionDescription}>
-          {this.state.loading ? 'Load!!' : 'loading...'}
-        </Text>
-        <View style={{marginBottom: 20}} />
+        <View style={styles.sectionContainer}>
+          <Text style={styles.sectionTitle}>Data status</Text>
+          <View style={{marginBottom: 20}}/>
+          <Text style={styles.sectionTitle}>Request</Text>
+          <Text style={styles.sectionDescription}>
+            {`Register: ${this.state.isRegistered}
+Permission: ${this.state.hasPermission}`}
+          </Text>
+          <View style={{marginBottom: 20}}/>
 
-        <Text style={styles.sectionTitle}>FCM token</Text>
-        <Text style={styles.sectionDescription}>
-          {this.state.loading ? this.state.fcmToken : 'loading...'}
-        </Text>
-        <View style={{marginBottom: 20}} />
+          <Text style={styles.sectionTitle}>FCM token</Text>
+          <Text selectable={true} style={styles.sectionDescription}>
+            {this.state.loading ? this.state.fcmToken : 'loading...'}
+          </Text>
+          <View style={{marginBottom: 20}}/>
 
-        <Text style={styles.sectionTitle}>APNs token</Text>
-        <Text style={styles.sectionDescription}>
-          {this.state.loading ? this.state.token : 'loading...'}
-        </Text>
+          <Text style={styles.sectionTitle}>APNs token</Text>
+          <Text selectable={true} style={styles.sectionDescription}>
+            {this.state.loading ? this.state.token : 'loading...'}
+          </Text>
 
-        <Button
-          title={'トークン更新'}
-          onPress={async () => {
-            try {
-              const fcmToken = await messaging().getToken();
-              const token = await messaging().getAPNSToken();
+          <Button
+              title={'トークン取得'}
+              onPress={async () => {
+                try {
+                  const fcmToken = await messaging().getToken();
+                  const token = await messaging().getAPNSToken();
 
-              this.setState({loading: true, fcmToken, token: token || 'null'});
-            } catch (e) {
-              Alert.alert('Error', e.message);
-              this.setState({loading: true, fcmToken: 'error', token: 'error'});
-            }
-          }}
-        />
-      </View>
+                  this.setState({loading: true, fcmToken, token: token || 'null'});
+                } catch (e) {
+                  Alert.alert('Error', e.message);
+                  this.setState({loading: true, fcmToken: 'error', token: 'error'});
+                }
+              }}
+          />
+
+          <Button
+              title={'registerForRemoteNotifications'}
+              onPress={async () => {
+                try {
+                  await messaging().registerForRemoteNotifications();
+                  const r = messaging().isRegisteredForRemoteNotifications;
+                  this.setState({
+                    isRegistered: r,
+                  });
+                  Alert.alert('INFO', `result is ${r}`);
+                } catch (e) {
+                  Alert.alert('Error', e.message);
+                  this.setState({isRegistered: false});
+                }
+              }}
+          />
+
+          <Button
+              title={'requestPermission'}
+              onPress={async () => {
+                try {
+                  const result = await messaging().requestPermission();
+                  this.setState({
+                    hasPermission: result,
+                  });
+                  Alert.alert('INFO', `result is ${result}`);
+                  const fcmToken = await messaging().getToken();
+                  const token = await messaging().getAPNSToken();
+
+                  this.setState({
+                    fcmToken,
+                    token: token || 'cannot get',
+                  });
+                } catch (e) {
+                  Alert.alert('Error', e.message);
+                  this.setState({hasPermission: false});
+                }
+              }}
+          />
+
+          {/*<Button*/}
+          {/*  title="Add To Basket"*/}
+          {/*  onPress={() =>*/}
+          {/*    analytics().logEvent('basket', {*/}
+          {/*      id: 3745092,*/}
+          {/*      item: 'mens grey t-shirt',*/}
+          {/*      description: ['round neck', 'long sleeved'],*/}
+          {/*      size: 'L',*/}
+          {/*    })*/}
+          {/*  }*/}
+          {/*/>*/}
+        </View>
     );
   }
 }
